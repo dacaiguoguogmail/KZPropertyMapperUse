06cceb6809b7dcaede2c551500ba3dbfd81a125a
dacaiguoguo <dacaiguoguo@163.com>
2013-11-19 10:15:28 +0800

    UIView (category)

---
+++ KZPropertyMapperUse/UIView+category.h
@@ -0,0 +1,13 @@
+//
+//  UIView+category.h
+//  KZPropertyMapperUse
+//
+//  Created by dacaiguoguo on 13-11-19.
+//  Copyright (c) 2013年 sunyanguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (category)
+
+@end
---
+++ KZPropertyMapperUse/UIView+category.m
@@ -0,0 +1,13 @@
+//
+//  UIView+category.m
+//  KZPropertyMapperUse
+//
+//  Created by dacaiguoguo on 13-11-19.
+//  Copyright (c) 2013年 sunyanguo. All rights reserved.
+//
+
+#import "UIView+category.h"
+
+@implementation UIView (category)
+
+@end
